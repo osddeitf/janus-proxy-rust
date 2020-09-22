2ea65f9da8178c233d0cc05c8aef1f2d0da2366a
osddeitf <osddeitf@gmail.com>
2020-09-22 15:51:08 +0700

    Remove serde::Serialize trait on request parameters types

--- src/janus/request.rs
+++ src/janus/request.rs
@@ -1,8 +1,8 @@
-use serde::{Serialize, Deserialize};
+use serde::Deserialize;
 use super::json::*;
 use serde_with::skip_serializing_none;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct IncomingRequestParameters {
 	pub transaction: JSON_STRING,	// JANUS_JSON_PARAM_REQUIRED
 	pub janus: JSON_STRING,				// JANUS_JSON_PARAM_REQUIRED
@@ -17,14 +17,14 @@ pub struct IncomingRequestParameters {
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct AttachParameters {
 	pub plugin: JSON_STRING,		// JANUS_JSON_PARAM_REQUIRED
 	pub opaque_id: Option<JSON_STRING>
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct BodyParameters {
 	pub body: JSON_OBJECT,
 	/** Unofficial property */
@@ -32,7 +32,7 @@ pub struct BodyParameters {
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct JsepParameters {
 	#[serde(rename = "type")]
 	pub _type: JSON_STRING, 		// JANUS_JSON_PARAM_REQUIRED
@@ -42,116 +42,116 @@ pub struct JsepParameters {
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct AddTokenParameters {
 	pub token: JSON_STRING, // JANUS_JSON_PARAM_REQUIRED
 	pub plugins: JSON_ARRAY<JSON_STRING>
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct TokenParameters {
 	pub token: JSON_STRING, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct AdminParameters {
 	pub transaction: JSON_STRING, // JANUS_JSON_PARAM_REQUIRED
 	pub janus: JSON_STRING, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct DebugParameters {
 	pub debug: JSON_BOOL, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct TimeoutParameters {
 	pub timeout: JSON_POSITIVE_INTEGER, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct LevelParameters {
 	pub level: JSON_POSITIVE_INTEGER, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct TimestampsParameters {
 	pub timestamps: JSON_BOOL, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct ColorsParameters {
 	pub colors: JSON_BOOL, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct MnqParameters {
 	pub min_nack_queue: JSON_POSITIVE_INTEGER, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct NmtParameters {
 	pub no_media_timer: JSON_POSITIVE_INTEGER, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct StParameters {
 	pub slowlink_threshold: JSON_POSITIVE_INTEGER, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct AnsParameters {
 	pub accept: JSON_BOOL, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct QueryHandlerParameters {
 	pub handler: JSON_STRING, // JANUS_JSON_PARAM_REQUIRED
 	pub request: JSON_OBJECT
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct QueryLoggerParameters {
 	pub logger: JSON_STRING, // JANUS_JSON_PARAM_REQUIRED
 	pub request: JSON_OBJECT
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct MessagePluginParameters {
 	pub plugin: JSON_STRING, // JANUS_JSON_PARAM_REQUIRED
 	pub request: JSON_OBJECT
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct CustomEventParamaters {
 	pub schema: JSON_STRING, // JANUS_JSON_PARAM_REQUIRED
 	pub data: JSON_OBJECT, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct CustomLoglineParameters {
 	pub line: JSON_STRING, // JANUS_JSON_PARAM_REQUIRED
 	pub level: JSON_POSITIVE_INTEGER
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct Text2pcapParameters {
 	pub folder: JSON_STRING,
 	pub filename: JSON_STRING,
@@ -159,19 +159,19 @@ pub struct Text2pcapParameters {
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct HandleInfoParameters {
 	pub plugin_only: JSON_BOOL
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct ResAddrParameters {
 	pub address: JSON_STRING, // JANUS_JSON_PARAM_REQUIRED
 }
 
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct TestStunParameters {
 	pub address: JSON_STRING, // JANUS_JSON_PARAM_REQUIRED
 	pub port: JSON_POSITIVE_INTEGER, // JANUS_JSON_PARAM_REQUIRED
